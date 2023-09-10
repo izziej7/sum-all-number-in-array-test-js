3ee6663c0c21eea3b21466ec166adc59b602c212
Fizza Jafri <jafrifm@gmail.com>
2023-09-10 14:31:48 -0400

    [feat]: implement sumItems function and update version of dependencies to avoid errors

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// Sum all the numbers in an array, including nested arrays
 function sumItems(array) {
-  // Sum all the numbers in the array
+  return array.reduce((sum, item) => {
+    // if the item is an array, add the result of sumItems to sum
+    // if the item is not an array, add the item to sum
+    return (Array.isArray(item)) ? sum + sumItems(item) : sum + item;
+  }, 0);
 }
 
 module.exports = sumItems;
